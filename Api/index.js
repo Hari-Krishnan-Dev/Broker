1329a527551b2c0887770a93df3ae25a9a886b4d
k.harikrishnan-8 <harikrishnan.k082001@gamil.com>
2024-06-19 17:09:47 +0530

    mangodb was succesfully connected

--- Api/index.js
+++ Api/index.js
@@ -1,6 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 const app =express();
 
+mongoose.connect(process.env.MONGO_URL)
+.then(()=>{
+    console.log("mangodb is connected")
+})
+.catch((err)=>{
+    console.log(err);
+})
 
 app.listen(4000,()=>{
     console.log("backend server is running on 4000 !!!");
